48a0fc4ccdeae22e8a38fe730615e11e55217d27
abukhmirov <alexbukhmirov@gmail.com>
2023-10-12 10:13:14 -0600

    Error and logging done

--- RecordCollection/Controllers/AlbumsController.cs
+++ RecordCollection/Controllers/AlbumsController.cs
@@ -49,32 +49,32 @@ namespace RecordCollection.Controllers
         [Route("/albums/{id:int}")]
         public IActionResult Delete(int? id)
         {
-            if (id == null)
+            if (id == null)                                                         //NULL CHECK FOR ID
             {
-                _logger.Warning("Album ID was null in Delete action.");
+                _logger.Warning("Album ID was null in Delete action.");             //LOG THE NULL ALBUM ID
                 return NotFound();
             }
 
             var album = _context.Albums.FirstOrDefault(a => a.Id == id);
 
-            if (album == null)
+            if (album == null)                                                      //NULL CHECK FOR ALBUM
             {
-                _logger.Warning($"Attempted to delete non-existing album with ID: {id}");
+                _logger.Warning($"Attempted to delete non-existing album with ID: {id}");       //LOG THE NULL ALBUM 
                 return NotFound();
             }
 
-            try
+            try                                                                      //TRY/CATCH FOR _CONTEXT.SAVECHANGES
             {
                 _context.Albums.Remove(album);
                 _context.SaveChanges();
 
-                _logger.Information($"Success! {album.Title} was removed from the database.");
+                _logger.Information($"Success! {album.Title} was removed from the database.");      //LOG THE SUCCESSFUL DELETION OF ALBUM
 
                 return RedirectToAction(nameof(Index));
             }
             catch (Exception)
             {
-                _logger.Fatal($"FATAL ERROR! Failed to delete album {album.Title}.");
+                _logger.Fatal($"FATAL ERROR! Failed to delete album {album.Title}.");               //LOG THE FAILURE TO DELETE ALBUM
                 return StatusCode(500);
             }
 
